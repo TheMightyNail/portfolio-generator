2a6cba8fb7f3c9de2baf67b71960e259d2a4afe2
Ian Linton <ilinton2571@gmail.com>
2022-01-19 21:14:36 -0700

    updated HTML output in page-template

--- app.js
+++ app.js
@@ -136,11 +136,11 @@ promptUser()
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //     if (err) throw new Error(err);
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
 
-        //     console.log('Page created! Checkout index.html in this directory to see it!');
-        // });
+            console.log('Page created! Checkout index.html in this directory to see it!');
+        });
     });
     
 
